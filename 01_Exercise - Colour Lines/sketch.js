c81d36a96e0761a3a64e6b4b5baae9414c3641f5
BTurner14 <44462090+BTurner14@users.noreply.github.com>
2018-10-25 12:48:40 +0100

    Add files via upload

---
+++ 01_Exercise - Colour Lines/sketch.js
@@ -0,0 +1,77 @@
+function setup() {
+  //Create the canvas
+  createCanvas(720, 400);
+  background(200);
+}
+  function draw() {
+
+  //Set colours
+  fill(0, 255, 255);
+  stroke(0, 255, 255);
+
+  //A line
+  line(700, 375, 20, 30);
+
+  //Set colours
+  fill(255, 165, 0);
+  stroke(255, 165, 0);
+
+  //A line
+  line(700, 35, 80, 340);
+
+  //Set colours
+  fill(0, 255, 0);
+  stroke(0, 255, 0);
+
+  //A line
+  line(50, 300, 50, 60);
+
+  //Set colours
+  fill(255, 255, 0);
+  stroke(255, 255, 0);
+
+  //A line
+  line(670, 300, 670, 60);
+
+  //Set colours
+  fill(255, 0, 255);
+  stroke(255, 0, 255);
+
+  //A line
+  line(900, 60, 100, 60);
+
+  //Set colours
+  fill(173, 255, 47);
+  stroke(173, 255, 47);
+
+  //A line
+  line(900, 350, 100, 350)
+
+  //Set colours
+  fill(0, 0, 255);
+  stroke(0, 0, 255);
+
+  //An ellipse
+  ellipse(50, 50, 100, 100);
+
+  //Set colours
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+
+  //An ellipse
+  ellipse(670, 50, 100, 100);
+
+  //Set colours
+  fill(0, 255, 0);
+  stroke(0,255,0);
+
+  //An ellipse
+  ellipse(670, 350, 100, 100);
+
+  //Set colours
+  fill(255, 255, 0);
+  stroke(255, 255, 0);
+
+  //An ellipse
+  ellipse(50, 350, 100, 100);
+}
