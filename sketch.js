eb5b541d98f5d08fe694ccd6fe2e74a5b6de7814
BTurner14 <44462090+BTurner14@users.noreply.github.com>
2018-10-30 18:08:56 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,123 @@
+  function setup(){
+    //Create the Canvas
+    createCanvas(800, 600)
+    background(255)
+}
+
+    function draw(){
+      //A rectangle
+      rect(680, 0, 40, 20)
+      fill(0)
+      stroke(0)
+     //A rectangle
+     rect(720, 20, 20, 40)
+     //A rectangle
+     rect(700, 60, 20, 40)
+     //A rectangle
+     rect(680, 100, 20, 60)
+     //A rectangle
+     rect(660, 160, 20, 20)
+     // A rectangle
+     rect(680, 180, 20, 20)
+     //A rectangle
+     rect(700, 200, 20, 20)
+     //A rectangle
+     rect(720, 220, 20, 20)
+     //A rectangle
+     rect(740, 240, 20, 20)
+     // A rectangle
+     rect(680, 240, 20, 20)
+
+     //A rectangle
+     rect(700, 260, 40, 20)
+     //A rectangle
+     rect(700, 280, 20, 20)
+     //A rectangle
+     rect(680, 300, 20, 20)
+     //A rectangle
+     rect(700, 320, 20, 80)
+     //A rectangle
+     rect(680, 400, 20, 60)
+     //A rectangle
+     rect(660, 460, 20, 20)
+     //A rectangle
+     rect(700, 460, 20, 20)
+     //A rectangle
+     rect(720, 480, 20, 20)
+     //A rectangle
+     rect(660, 500, 60, 20)
+     //A rectangle
+     rect(620, 480, 40, 20)
+     //A rectangle
+     rect(600, 460, 20, 20)
+     //A rectangle
+     rect(580, 420, 20, 40)
+
+     //A rectangle
+     rect(520, 400, 60, 20)
+     //A rectangle
+     rect(560, 320, 20, 80)
+     //A rectangle
+     rect(520, 340, 40, 20)
+     //A rectangle
+     rect(540, 320, 20, 20)
+     //A rectangle
+     rect(580, 300, 60, 20)
+
+     //A rectangle
+     rect(420, 520, 240, 20)
+     //A rectangle
+     rect(400, 500, 20, 20)
+     //A rectangle
+     rect(420, 360, 20, 140)
+     //A rectangle
+     rect(440, 360, 20, 20)
+     //A rectangle
+     rect(460, 320, 20, 40)
+     //A rectangle
+     rect(420, 300, 40, 20)
+     //A rectangle
+     rect(400, 320, 20, 40)
+     //A rectangle
+     rect(420, 260, 20, 40)
+     //A rectangle
+     rect(440, 240, 20, 20)
+
+     //A rectangle
+     rect(460, 260, 220, 20)
+     //A rectangle
+     rect(460, 220, 20, 20)
+     //A rectangle
+     rect(480, 200, 20, 80)
+     //A rectangle
+     rect(500, 180, 60, 20)
+     //A rectangle
+     rect(520, 200, 100, 20)
+     //A rectangle
+     rect(580, 220, 80, 20)
+     //A rectangle
+     rect(580, 240, 80, 20)
+     //A rectangle
+     rect(500, 280, 60, 20)
+     //A rectangle
+     rect(500, 240, 60, 20)
+     //A rectangle
+     rect(520, 220, 40, 20)
+
+     //A rectangle
+     rect(440, 160, 60, 20)
+     //A rectangle
+     rect(420, 140, 20, 20)
+     //A rectangle
+     rect(400, 120, 20, 20)
+     //A rectangle
+     rect(420, 100, 120, 20)
+     //A rectangle
+     rect(540, 80, 40, 20)
+     //A rectangle
+     rect(580, 60, 40, 20)
+     //A rectangle
+     rect(620, 40, 20, 20)
+     //A rectangle
+     rect(640, 20, 40, 20)
+    }
